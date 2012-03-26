f7dad37821307c8c32ecbfa517babe9dbe989728
farrell.damien <farrell.damien@fd2c4fec-ed7f-fb94-74fb-8070d365187d>
2012-03-26 18:50:36 +0000

    dist tools changes

--- TableModels.py
+++ TableModels.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python
-#
-# Protein Engineering Analysis Tool DataBase (PEATDB)
-# Copyright (C) 2010 Damien Farrell & Jens Erik Nielsen
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Contact information:
-# Email: Jens.Nielsen_at_gmail.com 
-# Normal mail:
-# Jens Nielsen
-# SBBS, Conway Institute
-# University College Dublin
-# Dublin 4, Ireland
-# 
+"""
+    Created Oct 2008
+    TablePlotter Class
+    Copyright (C) Damien Farrell
+ 
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+ 
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+ 
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
 
 from TableFormula import Formula
 from types import *
--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from distutils.core import setup
+import sys,os
+home=os.path.expanduser('~')
+pythonpath=os.path.join(home,'python')
 
 setup(
     name = 'tkintertable',
     version = '1.00',
-    description = 'Extendable table class for tkinter.',
-    author = 'Damien Farrell',
-    author_email =  'damien.farrell@ucd.ie',
+    description = 'Extendable table class for Tkinter',
+    url='http://code.google.com/p/tkintertable/',
+    license='GPL v3',
+    author = 'Damien Farrell',    
+    author_email = 'farrell.damien[at]gmail.com',
+    #package_dir = {'':'../'},
+    packages = ['tkintertable'],
+    #py_modules = ['tkintertable'],
+    requires=['Tkinter'],
 )
