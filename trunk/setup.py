6180e94d6f95449576ba35723bce62a1706fdc8b
farrell.damien <farrell.damien@fd2c4fec-ed7f-fb94-74fb-8070d365187d>
2012-04-28 11:08:34 +0000

    updated setup.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,18 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
 import sys,os
 home=os.path.expanduser('~')
-pythonpath=os.path.join(home,'python')
 
 setup(
     name = 'tkintertable',
-    version = '1.0.0',
+    version = '1.0.1',
     description = 'Extendable table class for Tkinter',
     url='http://code.google.com/p/tkintertable/',
     license='GPL v3',
     author = 'Damien Farrell',    
-    author_email = 'farrell.damien[at]gmail.com',
-    #package_dir = {'':'../'},
-    packages = ['tkintertable'],
-    #py_modules = ['tkintertable'],
-    requires=['Tkinter'],
+    author_email = 'farrell.damien[at]gmail.com',   
+    packages = ['tkintertable'], 
+    dependency_links = [
+          "http://download.sourceforge.net/pmw/Pmw.1.3.tar.gz"],
+    entry_points = { 'gui_scripts': [
+                     'tablesapp = tkintertable.Tablesapp:main']}
 )
